88c035a304a6dffc4e0448c3e5ffb87b0bbac106
QuantumT95 <tran.charlie@ymail.com>
2018-10-28 00:27:46 -0700

    Spelling Check

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hello World! Hi people!</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>My name is Sean! I am a big fan of freecodecamp. It was the first code resource I found when I discovered the world of coding.</p>
+    <p>My name is Sean! I am a big fan of FreeCodeCamp. It was the first code resource I found when I discovered the world of coding.</p>
     <p>A year later, I find myself in the bay area studying C full-time. I aspire to be the very best!</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
